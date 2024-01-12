25579c36478153031c903ac59cd6129afb06c67e
Serah Nderi <sarahngima77@gmail.com>
2024-01-12 09:55:59 +0300

    Added JavaScript Styling

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Weather from './Weather'
+
+function App() {
+
+  return (
+    <>
+    <div className='App'>
+      <div className='container'>
+        <Weather /> 
+        <footer>
+          This project was coded by {" "}
+          <a href=" https://www.linkedin.com/in/serah-nderi/" target='_blank'>Serah Nderi</a> and is {" "}
+          <a href=" https://github.com/MundiaNderi/weather-app-react" target='_blank'>Open-sourced on Github</a>
+        </footer>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
