c1859fe9bd680c745a23ad827cee71a1d83c87fd
Serah Nderi <sarahngima77@gmail.com>
2024-01-15 10:36:39 +0300

    Weather Icon Component

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -18,7 +18,7 @@ const [city, setCity] = useState(props.defaultCity);
             city: response.data.name,
             description: response.data.weather[0].description,
             date: new Date(response.data.dt * 1000),
-            iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+            icon: response.data.weather[0].icon,
         })
     }
     function Search() {
